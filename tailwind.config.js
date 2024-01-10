3187ff76bf76bd9c8c3fd2bc72e8bcd67dee20a8
Lucas Larangeira <lukearch.sh@gmail.com>
2024-01-10 12:31:55 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = {
           DEFAULT: "#40C634",
         },
       },
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          DEFAULT: "1280px",
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
   },
   plugins: [],
